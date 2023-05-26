e6b03432d16b99278a78ed9af2d3e7b1b2cf5b61
Othmane <gassab.othmane&@gmail.com>
2023-05-26 08:21:02 -0700

    added 0x16. C - Simple Shell files

--- shell.h
+++ shell.h
@@ -1,58 +1,80 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <string.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
-#include <stddef.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
 
-extern char **environ;
+/**
+ * struct variables - variables
+ * @av: command line arguments
+ * @buffer: buffer of command
+ * @env: environment variables
+ * @count: count of commands entered
+ * @argv: arguments at opening of shell
+ * @status: exit status
+ * @commands: commands to execute
+ */
+typedef struct variables
+{
+	char **av;
+	char *buffer;
+	char **env;
+	size_t count;
+	char **argv;
+	int status;
+	char **commands;
+} vars_t;
 
-/* exit_func.c */
-void ext_func(char **argv, char *ptr_line, int ext);
-void get_envi(char **env);
+/**
+ * struct builtins - struct for the builtin functions
+ * @name: name of builtin command
+ * @f: function for corresponding builtin
+ */
+typedef struct builtins
+{
+	char *name;
+	void (*f)(vars_t *);
+} builtins_t;
 
-/* fork_func.c */
-int fork_func(char *arg[], char *av[], char *envi[],
-		char *ptr_line, int pidd, int checker);
+char **make_env(char **env);
+void free_env(char **env);
 
-/* get_path_func.c */
-char *get_path(char **env);
+ssize_t _puts(char *str);
+char *_strdup(char *strtodup);
+int _strcmpr(char *strcmp1, char *strcmp2);
+char *_strcat(char *strc1, char *strc2);
+unsigned int _strlen(char *str);
 
-/* get_token_func.c */
-char **get_token(char *ptr_line);
+char **tokenize(char *buffer, char *delimiter);
+char **_realloc(char **ptr, size_t *size);
+char *new_strtok(char *str, const char *delim);
 
-/* get_cmd_func.c */
-char *getline_cmd(void);
+void (*check_for_builtins(vars_t *vars))(vars_t *vars);
+void new_exit(vars_t *vars);
+void _env(vars_t *vars);
+void new_setenv(vars_t *vars);
+void new_unsetenv(vars_t *vars);
 
-/* ptr_cmd.c */
-void prt_cmd(void);
+void add_key(vars_t *vars);
+char **find_key(char **env, char *key);
+char *add_value(char *key, char *value);
+int _atoi(char *str);
 
-/* path_seperator.c */
-int path_sepetator(char **arg, char **env);
+void check_for_path(vars_t *vars);
+int path_execute(char *command, vars_t *vars);
+char *find_path(char **env);
+int execute_cwd(vars_t *vars);
+int check_for_dir(char *str);
 
-/* prt_cmd.c */
-void prt_cmd(void);
+void print_error(vars_t *vars, char *msg);
+void _puts2(char *str);
+char *_uitoa(unsigned int count);
 
-/* Helper functions */
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
-
-int _strncmp(const char *s1, const char *s2, size_t n);
-char *_strcpy(char *dest, const char *src);
-
-char *_strtok(char *str, const char *delim);
-size_t _strcspn(char *s1, char *s2);
-
-unsigned int _strspn(char *s, char *accept);
-char *_strchr(char *s, char c);
-char *_strdup(const char *src);
-#endif
+#endif /* _SHELL_H_ */
